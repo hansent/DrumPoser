f04d03cb967a87029293416d8984ea4932b8608e
A.J. Venter <ajventer@gmail.com>
2012-03-27 17:16:10 +0200

    Dictionary based screens

---
+++ main.py
@@ -0,0 +1,45 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+
+
+class AppScreen(FloatLayout):
+    app = ObjectProperty(None)
+
+
+class EditorScreen(AppScreen):
+    beat = ObjectProperty(None)
+
+class LoadScreen(AppScreen):
+    pass
+
+class TeachMeScreen(AppScreen):
+    pass
+
+class MainMenu(AppScreen):
+    def new_beat(self):
+        new_beat = {"name": "New Beat"}
+        self.app.screens["editor"].beat = new_beat
+        self.app.goto_screen("editor")
+
+
+class DrumPoserApp(App):
+    def build(self):
+        self.screens = {}
+        self.screens["editor"] = EditorScreen(app=self)
+        self.screens["load"] = LoadScreen(app=self)
+        self.screens["teachme"] = TeachMeScreen(app=self)
+        self.screens["menu"] = MainMenu(app=self)
+        self.root = FloatLayout()
+        self.goto_screen("menu")
+        return self.root
+
+    def goto_screen(self, screen_name):
+        self.root.clear_widgets()
+        self.root.add_widget(self.screens[screen_name])
+
+  
+if __name__ == '__main__':
+    DrumPoserApp().run()
