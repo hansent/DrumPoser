28c51358e175d19af92ec5c18a3fc5ea1e0841d7
Thomas Hansen <thomas.hansen@gmail.com>
2012-03-29 12:53:23 +0200

    compose editor screen of separate custom widget types
    compose EditorScreen of:
    EditorSettings (top bar)
    Editor Controlls (lay/pasue, etc.)
    InstrumentEditor (one instrument = one row)

--- main.py
+++ main.py
@@ -2,32 +2,64 @@ import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, NumericProperty, ListProperty, StringProperty
 from kivy.uix.label import Label
+from kivy.factory import Factory as F
 
 
 class AppScreen(FloatLayout):
     app = ObjectProperty(None)
 
 
-class EditorScreen(AppScreen):
-    instrument_list_wid = ObjectProperty(None)
+class MainMenu(AppScreen):
+    pass
 
-    def draw_instrument_list(self):
-        print "resetting instrument list"
-        self.instrument_list_wid.clear_widgets()
-        label1 = Label(text='Open High Hat', font_size=20)
-        self.instrument_list_wid.add_widget(label1)
-
-    def init_editor(self):
-        self.draw_instrument_list()
 
 class TeachMeScreen(AppScreen):
     pass
 
-class MainMenu(AppScreen):
+
+class EditorScreen(AppScreen):
+    instrument_list = ObjectProperty(None)
+    settings = ObjectProperty(None)
+    controlls = ObjectProperty(None)
+
+
+    def init_editor(self):
+        self.instrument_list.clear_widgets()
+        for instr_name in open("instruments.txt", "r").readlines():
+            instr = InstrumentEditor(name=instr_name)
+            self.instrument_list.add_widget(instr)
+
+
+    def play(self):
+        print "PLAY"
+
+    def pause(self):
+        print "PLAY"
+
+    def previous(self):
+        print "PREVIOUS"
+
+    def next(self):
+        print "NEXT"
+
+
+class EditorSettings(BoxLayout):
+    time_signature = NumericProperty(4.0)
+    tempo = NumericProperty(4.0)
+
+
+
+class EditorControlls(BoxLayout):
     pass
+
+
+class InstrumentEditor(BoxLayout):
+    name = StringProperty("Instrument")
+
+
+
 
 class DrumPoserApp(App):
     def build(self):
@@ -44,7 +76,12 @@ class DrumPoserApp(App):
         self.root.add_widget(self.screens[screen_name])
         if screen_name == 'editor':
             self.screens["editor"].init_editor()
-            
-  
+
+
+
+F.register("EditorSettings", EditorSettings)
+F.register("EditorControlls", EditorControlls)
+F.register("InstrumentEditor", InstrumentEditor)
+
 if __name__ == '__main__':
     DrumPoserApp().run()
